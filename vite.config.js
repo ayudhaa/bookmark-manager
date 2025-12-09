a8a1e8307a0f49af62ebde8186251d1c159675ca
ayudha <ayudharachman@gmail.com>
2025-12-09 15:14:30 +0700

    merge enhanced

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    })
+  ],
   base: '/bookmark-manager/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
